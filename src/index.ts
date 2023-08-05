6aabe6a6b88585592d6b78514211576d4db2b020
csturgeon <sturgeon.chris@gmail.com>
2023-08-05 17:30:02 +0100

    Add layout

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const item = document.querySelector('.item');
 
-import confetti from 'canvas-confetti';
+function handleDragStart(e: Event) {
+  console.log(e);
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+item!.addEventListener('dragstart', handleDragStart);
